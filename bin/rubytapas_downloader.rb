62d70f6a1f3ddab3159c91657a29e0c98778636e
Tonći Galić <tuxified@gmail.com>
2015-01-21 23:28:45 +0100

    Inital version RubyTapas downloader

---
+++ bin/rubytapas_downloader.rb
@@ -0,0 +1,98 @@
+#!/usr/bin/env ruby
+
+require 'httparty'
+require 'nokogiri'
+
+USERNAME = ENV['RTAPAS_USERNAME'] || "email-used@in-registration.com"
+PASSWORD = ENV['RTAPAS_PASSWORD'] || "your-password-here"
+DOWNLOAD_DIR = ARGV.pop || File.dirname(__FILE__)
+COOKIE_FILE = 'cookies.txt' # by example
+
+# Adapted version of: https://github.com/cibernox/rubytapas_downloader
+class RubytapasDownloader
+  FEED_URL  = "https://rubytapas.dpdcart.com/feed"
+  LOGIN_URL = "http://rubytapas.dpdcart.com/subscriber/login?__dpd_cart=8f511233-b72b-4a8c-8c37-fadc74fbc3a1"
+
+  ##
+  # Fetchs and parses the rss feed. Generates the episodes
+  #
+  def initialize
+    rss = HTTParty.get(FEED_URL, basic_auth: { username: USERNAME, password: PASSWORD })
+    @episodes = Nokogiri::XML(rss).css('item').map{ |item| Episode.new(item) }
+  end
+
+  ##
+  # Downloads the new episodes with curl.
+  #
+  def launch
+    puts "--- LAUNCHING RUBYTAPAS DOWNLOADER ---"
+
+    puts "--- LOG IN AND SAVE COOKIE ---"
+    login_and_save_cookie
+
+    new_episodes = @episodes.reject(&:downloaded?)
+    count = new_episodes.size
+    puts "#{count} NEW EPISODES"
+
+
+    new_episodes.each_with_index do |episode, index|
+      puts "DOWNLOADING #{episode.title} (#{index + 1} of #{count})"
+      episode.download!
+    end
+
+    puts "--- FINISHED RUBYTAPAS DOWNLOADER ---"
+  rescue Exception => e
+    puts "--- EXCEPTION RAISED WHILE DOWNLOADING --"
+    puts e.inspect
+  ensure
+    File.delete(COOKIE_FILE) if File.exist?(COOKIE_FILE)
+  end
+
+private
+
+  def login_and_save_cookie
+    system %Q{curl -c #{COOKIE_FILE} -d "username=#{USERNAME}&password=#{PASSWORD}" #{LOGIN_URL}}
+  end
+
+end
+
+
+class Episode
+  attr_accessor :title, :files
+
+  ##
+  # Extracts informations from the parsed XML node
+  #
+  def initialize(parsed_rss_item)
+    @title = parsed_rss_item.css('title').text << ".mp4" #.gsub(/\s|\W/, '-').gsub('--','-')
+    @files = {}
+    parsed_description = Nokogiri::XML(parsed_rss_item.css('description').text)
+    parsed_description.css('a').each do |link|
+      @files[link.text] = link[:href]
+    end
+  end
+
+  # TODO: find out if file was downloaded completely
+  def downloaded?
+    File.exist?(File.join(DOWNLOAD_DIR, title))
+  end
+
+  def download!
+    verify_download_dir!
+    files.each do |filename, url|
+      next unless filename =~ /.+\.mp4$/
+      file_path = File.join(DOWNLOAD_DIR, title)
+      system %Q{curl -o "#{file_path}" -b #{COOKIE_FILE} -d "username=#{USERNAME}&password=#{PASSWORD}" #{url}}
+    end
+  end
+
+  def verify_download_dir!
+    return true if Dir.exists?(DOWNLOAD_DIR)
+    require 'fileutils'
+    FileUtils.mkdir_p(DOWNLOAD_DIR)
+  end
+end
+
+if __FILE__ == $0
+  RubytapasDownloader.new.launch
+end
